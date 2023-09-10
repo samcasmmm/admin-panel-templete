a56dc724d443c199bda29acd6395919567d229cd
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-09-10 21:27:12 +0530

    Manage Leads and Lead Card

--- src/components/LeadCard.tsx
+++ src/components/LeadCard.tsx
@@ -21,7 +21,7 @@ const LeadCard: React.FC<Props> = ({
   action,
 }) => {
   return (
-    <div className="w-full my-4 bg-white dark:bg-boxdark p-4 rounded-lg flex items-start flex-col gap-3">
+    <div className="w-full my-4 bg-white dark:bg-boxdark p-4 rounded-lg flex items-start flex-col gap-3 shadow-2">
       <p className="dark:text-primary dark:hover:text-blue-800 font-bold text-primary hover:text-blue-800 cursor-pointer">
         {name}
         <span className="bg-warning/20 px-2 py-1 rounded-full font-bold text-[12px] text-warning ml-2">
@@ -33,7 +33,7 @@ const LeadCard: React.FC<Props> = ({
         <span className="font-bold bg-white dark:bg-boxdark">{broker}</span>
       </p>
       <p>
-        Lead assigned to : <span>{leadAssigned}</span>
+        Lead assigned to<span className="ml-2 font-bold">{leadAssigned}</span>
       </p>
       <p className="px-4 py-2 bg-purple-300/20 bg-boxdark-2 w-35 text-center rounded-full text dark:text-white text-black font-bold text-sm">
         {propertyName}
