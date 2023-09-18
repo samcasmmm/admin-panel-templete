3c882933087c87184a40762da45cd6bf136b8ea0
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-09-18 22:02:38 +0530

    custom input element

---
+++ src/components/InputElement.tsx
@@ -0,0 +1,41 @@
+import React, { useRef, useState } from 'react';
+
+type Props = {
+  label: string;
+  onChange?: () => (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+const InputElement = ({ label, onChange }: Props) => {
+  const [focused, setFocused] = useState<Boolean>(false);
+
+  return (
+    <div className="relative flex flex-col">
+      <label
+        htmlFor=""
+        className={`absolute left-4 ${
+          focused ? 'top-4' : 'top-[-12px]'
+        } bg-white px-3 font-semibold
+         dark:bg-transparent `}
+      >
+        {label}
+      </label>
+      <input
+        onChange={(e) => {
+          onChange(e);
+        }}
+        id=""
+        name=""
+        value=""
+        onFocus={() => {
+          setFocused(false);
+        }}
+        onBlur={() => {
+          setFocused(true);
+        }}
+        className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary"
+      />
+    </div>
+  );
+};
+
+export default InputElement;
--- src/pages/manageUsers/ManageUser.tsx
+++ src/pages/manageUsers/ManageUser.tsx
@@ -1,5 +1,31 @@
+import Breadcrumb from '../../components/Breadcrumb';
+import InputElement from '../../components/InputElement';
+import { useState } from 'react';
+
 const ManageUser = () => {
-  return <div>ManageUser</div>;
+  const [inputData, setInputData] = useState({
+    empName: '',
+    empEmail: '',
+    empNumber: '',
+    empOTP: '',
+    empUserType: '',
+    empPermissions: '',
+    empProperty: '',
+    empReportingManager: '',
+  });
+  const handleChange = (e) => {};
+  return (
+    <>
+      <Breadcrumb pageName="ManageUser" altPageName="Manage User" />
+      <div className="min-h-[100px] rounded-md bg-white dark:bg-black">
+        <p className="p-3 font-bold">Add Users</p>
+        <div className="my-2 border-b-[1px] border-b-slate-300 dark:border-b-boxdark" />
+        <div className="p-3">
+          <InputElement label="Name" onChange={(e) => handleChange(e)} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ManageUser;
